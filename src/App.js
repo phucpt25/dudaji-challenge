9b952646db6c47859466867b2955ba0fd0de99fa
Phúc <68469045+phucpt25@users.noreply.github.com>
2024-08-06 13:24:20 +0700

    [P] adding first logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import io from 'socket.io-client';
 import './App.css';
 
+const socket = io.connect('http://localhost:5001')
+
 function App() {
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState(['123']);
+
+  useEffect(() => {
+    socket.on('message', (m) => {
+      setMessages((oldMessages) => [...oldMessages, m]);
+    })
+
+    return () => {
+      socket.off('message');
+    }
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit('message', message);
+    setMessage('');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Dudaji challenge</h1>
+      <div className="messages">
+        {messages.map((m, i) => (
+          <div key={i}>{m}</div>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type='text'
+          placeholder='...'
+          value={message}
+          onChange={(event) => {setMessage(event.target.value)}}
+        />
+        <button type="submit">Sent</button>
+      </form>
     </div>
-  );
+   );
 }
 
 export default App;
